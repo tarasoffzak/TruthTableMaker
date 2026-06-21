d2e97307c3ecc317be97a37fcd26956cb1a04b1a
Zakhar Tarasoff <tarasoffzak@gmail.com>
2026-06-21 23:56:31 +0300

    refactor: make printTable use writeCSV

--- TruthTableMaker/src/TableOutput.cpp
+++ TruthTableMaker/src/TableOutput.cpp
@@ -28,13 +28,7 @@ void printTable(
     const std::vector<std::string>& vars,
     const std::vector<std::pair<Context, bool>>& table)
 {
-    const size_t n = vars.size();
-    for (size_t i = 0; i < n; ++i) std::cout << vars[i] << '\t';
-    std::cout << "result\n";
-    for (const auto& [ctx, val] : table) {
-        for (size_t i = 0; i < n; ++i) std::cout << ((ctx >> i) & 1) << '\t';
-        std::cout << (val ? 1 : 0) << '\n';
-    }
+    writeCSV(std::cout, '\t', vars, table);
 }
 
 void saveToFile(
